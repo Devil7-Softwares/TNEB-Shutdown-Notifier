8239289f344272c1d02581d55ce62d9716167faf
Devil7DK <dineshthangavel47@gmail.com>
2023-12-16 21:52:47 +0530

    Implement schedule scrapping

--- TNEB.Shutdown.Notifier.Web/Jobs/ScrapperJob.cs
+++ TNEB.Shutdown.Notifier.Web/Jobs/ScrapperJob.cs
@@ -8,16 +8,152 @@ namespace TNEB.Shutdown.Notifier.Web.Jobs
     public class ScheduleScrapperJob : IJob
     {
         private readonly ILogger<ScheduleScrapperJob> logger;
+        private readonly AppDbContext dbContext;
 
-        public ScheduleScrapperJob(ILogger<ScheduleScrapperJob> logger)
+        public ScheduleScrapperJob(ILogger<ScheduleScrapperJob> logger, AppDbContext dbContext)
         {
             this.logger = logger;
+            this.dbContext = dbContext;
         }
 
-        public Task Execute(IJobExecutionContext context)
+        private async Task<Location> GetLocation(string locationName)
         {
-            logger.LogInformation("ScrapperJob started");
-            return Task.CompletedTask;
+            Location? location = dbContext.Locations.FirstOrDefault(l => l.Name == locationName);
+
+            if (location == null)
+            {
+                LocationStandardization? locationStandardization = dbContext.LocationStandardization.FirstOrDefault(l => l.Location == locationName);
+
+                if (locationStandardization != null)
+                {
+                    location = dbContext.Locations.FirstOrDefault(l => l.Name == locationStandardization.StandardizedLocation);
+                }
+
+                if (location == null)
+                {
+                    logger.LogDebug($"Adding location {locationName}...");
+
+                    location = new Location
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = locationStandardization?.StandardizedLocation ?? locationName
+                    };
+
+                    dbContext.Locations.Add(location);
+
+                    await dbContext.SaveChangesAsync<Location>();
+                    logger.LogDebug($"Location {locationName} added!");
+                }
+            }
+
+            return location;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            CircleEntry[] circleEntries = Array.Empty<CircleEntry>();
+
+            try
+            {
+                logger.LogDebug("Fetching circles for fetching schedules...");
+                circleEntries = dbContext.Circles.ToArray();
+                logger.LogInformation($"{circleEntries.Length} Circles fetched for fetching schedules!");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Failed to fetch circles for fetching schedules!");
+                return;
+            }
+
+            if (circleEntries.Length == 0)
+            {
+                logger.LogWarning("No circles found for fetching schedules!");
+                return;
+            }
+
+            Dictionary<CircleEntry, ISchedule[]> circleSchedule = new Dictionary<CircleEntry, ISchedule[]>();
+
+            for (int i = 0; i < circleEntries.Length; i++)
+            {
+                CircleEntry circleEntry = circleEntries[i];
+                logger.LogDebug($"[{i + 1}/{circleEntries.Length}] Fetching schedules for circle {circleEntry.Name} ({circleEntry.Value})...");
+
+                try
+                {
+                    ISchedule[] circleSchedules = await Utils.GetSchedules(circleEntry.Value);
+                    logger.LogInformation($"[{i + 1}/{circleEntries.Length}] {circleSchedules.Length} Schedules fetched for circle {circleEntry.Name} ({circleEntry.Value})!");
+
+                    circleSchedule.Add(circleEntry, circleSchedules);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, $"[{i + 1}/{circleEntries.Length}] Failed to fetch schedules for circle {circleEntry.Name} ({circleEntry.Value})!");
+                    continue;
+                }
+            }
+
+            foreach (KeyValuePair<CircleEntry, ISchedule[]> keyValue in circleSchedule)
+            {
+                CircleEntry circle = keyValue.Key;
+                ISchedule[] schedules = keyValue.Value;
+
+                foreach (ISchedule schedule in schedules)
+                {
+                    string[] locationNames = schedule.Location.Split(',').Select(l => l.Trim()).TakeWhile(l => !string.IsNullOrWhiteSpace(l)).ToArray();
+
+                    foreach (string locationName in locationNames)
+                    {
+                        Location location = await GetLocation(locationName);
+
+                        ScheduleEntry? existingScheduleEntry = dbContext.Schedules.FirstOrDefault(s => s.Date == schedule.Date && s.From == schedule.From && s.To == schedule.To && s.LocationId == location.Id && s.CircleId == circle.Id);
+
+                        if (existingScheduleEntry == null)
+                        {
+                            logger.LogDebug($"Adding schedule for {location.Name} ({schedule.Date.ToString("yyyy-MM-dd")})...");
+
+                            ScheduleEntry scheduleEntry = new ScheduleEntry
+                            {
+                                Id = Guid.NewGuid(),
+                                Date = schedule.Date,
+                                From = schedule.From,
+                                To = schedule.To,
+                                Town = schedule.Town,
+                                SubStation = schedule.SubStation,
+                                Feeder = schedule.Feeder,
+                                TypeOfWork = schedule.TypeOfWork,
+                                LocationId = location.Id,
+                                CircleId = circle.Id
+                            };
+
+                            dbContext.Schedules.Add(scheduleEntry);
+
+                            logger.LogInformation($"Schedule for {location.Name} ({location.Id}) added!");
+                        }
+                        else
+                        {
+                            logger.LogDebug($"Schedule for {location.Name} ({location.Id}) already exists!");
+                        }
+                    }
+                }
+            }
+
+            try
+            {
+                if (dbContext.ChangeTracker.HasChanges())
+                {
+                    logger.LogDebug("Saving schedules...");
+                    await dbContext.SaveChangesAsync();
+                    logger.LogInformation("Schedules saved!");
+                }
+                else
+                {
+                    logger.LogInformation("No schedules changed to save!");
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Failed to save schedules!");
+            }
         }
     }
 

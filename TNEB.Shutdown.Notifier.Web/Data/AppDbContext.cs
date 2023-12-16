49223ebdcf0711b1b61aa4329da6a6b078ca00da
Devil7DK <dineshthangavel47@gmail.com>
2023-12-16 21:47:37 +0530

    Implement overload method in AppDbContext to save changes of specific entity

--- TNEB.Shutdown.Notifier.Web/Data/AppDbContext.cs
+++ TNEB.Shutdown.Notifier.Web/Data/AppDbContext.cs
@@ -24,5 +24,43 @@ namespace TNEB.Shutdown.Notifier.Web.Data
             scheduleEntryBuilder.Property(e => e.From).HasConversion(Converters.DateTimeOffsetToUnix);
             scheduleEntryBuilder.Property(e => e.To).HasConversion(Converters.DateTimeOffsetToUnix);
         }
+
+        /// <summary>
+        /// Save changes to the database without affecting other entities
+        /// </summary>
+        /// <typeparam name="TEntity">
+        /// The type of entity to save changes for
+        /// </typeparam>
+        /// Source: https://stackoverflow.com/a/33404229
+        public async Task<int> SaveChangesAsync<TEntity>() where TEntity : class
+        {
+            var original = this.ChangeTracker.Entries()
+                        .Where(x => !typeof(TEntity).IsAssignableFrom(x.Entity.GetType()) && x.State != EntityState.Unchanged)
+                        .GroupBy(x => x.State)
+                        .ToList();
+
+            foreach (var entry in this.ChangeTracker.Entries().Where(x => !typeof(TEntity).IsAssignableFrom(x.Entity.GetType())))
+            {
+                entry.State = EntityState.Unchanged;
+            }
+
+            var rows = await base.SaveChangesAsync();
+
+            foreach (var state in original)
+            {
+                foreach (var entry in state)
+                {
+                    entry.State = state.Key;
+                }
+            }
+
+            return rows;
+        }
+
+        public int SaveChanges<TEntity>() where TEntity : class
+        {
+            return SaveChangesAsync<TEntity>().GetAwaiter().GetResult();
+        }
+
     }
 }
